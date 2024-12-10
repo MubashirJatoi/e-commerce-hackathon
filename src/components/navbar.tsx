ab0d3648b0bd0319ade97b597c0646786139f293
Mubashir Hussain <124498517+MubashirJatoi@users.noreply.github.com>
2024-12-11 01:28:35 +0500

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,13 +3,16 @@ import Nike from "../../public/Images/nike-logo.png"
 import Like from "../../public/Images/like.png"
 import Buy from "../../public/Images/buy.png"
 import Search from "../../public/Images/search.png"
+import Link from "next/link";
 
 function Navbar() {
     return(
         <div className="flex w-[100%] h-[60px] items-center">
             <div className="flex items-center justify-around h-[60px] w-[95%] m-auto">
             <div>
+            <Link href={"/"}>
                 <Image className="cursor-pointer" src={Nike} alt="nike"></Image>
+            </Link>
             </div>
             <div className="flex text-[15px] ml-[130px] font-[600] gap-6">
                 <p className="cursor-pointer hover:underline">New & Featured</p>
